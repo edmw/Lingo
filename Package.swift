52c1afb3a10335af97b8444a3456bec036af3090
Michael <mib@michael-baumgaertner.de>
2020-02-25 23:34:54 +0100

    add swift-log dependency

--- Package.swift
+++ Package.swift
@@ -2,7 +2,14 @@ import PackageDescription
 
 let package = Package(
     name: "Lingo",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+    ],
     targets: [
-        Target(name: "Lingo"),
+        Target(
+            name: "Lingo",
+            dependencies: [ "Logging" ]
+        )
     ]
 )
+
